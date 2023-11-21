d0974487be9f826f6c9ffe9e91e2f51b02e7b159
Lucas Costabile <111201828+LucasCostabile@users.noreply.github.com>
2023-11-21 15:39:42 -0300

    adicionando componentes
    Componente ItemsContainer e CartWidget

---
+++ src/app/components/ItemsContainer/ItemListContainer.tsx
@@ -0,0 +1,17 @@
+
+interface ItemListContainerProps {
+  greeting: string;
+}
+
+
+
+const ItemListContainer: React.FC<ItemListContainerProps> = ({ greeting }) => {
+  return (
+    <div className="text-center">
+      <h2>{greeting}</h2>
+    </div>
+  );
+};
+
+export { ItemListContainer };
+
