0399ccd78d3710ab3ae74db6916ee06535f31c87
Lucas Costabile <111201828+LucasCostabile@users.noreply.github.com>
2023-11-22 21:43:35 -0300

    new component ItemCount

---
+++ src/app/components/Count/ItemCount.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+
+interface ItemCountProps {
+  initial: number;
+  stock: number;
+  onAdd: (quantity: number) => void;
+}
+
+const ItemCount: React.FC<ItemCountProps> = ({ initial, stock, onAdd }) => {
+  const [count, setCount] = useState(initial);
+
+  const handleIncrement = () => {
+    if (count < stock) {
+      setCount(count + 1);
+    }
+  };
+
+  const handleDecrement = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    }
+  };
+
+  const handleAddToCart = () => {
+    if (stock > 0 && count > 0) {
+      onAdd(count);
+    }
+  };
+
+  console.log("Quantidade selecionada:", count); // Adiciona log da quantidade selecionada
+
+  return (
+    <div>
+      <div className=" flex justify-around items-center w-32 h-12  border border-gray-300 rounded-md font-normal">
+        <button className=" w-6 h-6 bg-slate-400 rounded-full " onClick={handleDecrement}>-</button>
+        <span>{count}</span>
+        <button className=" w-6 h-6 bg-slate-400 rounded-full " onClick={handleIncrement}>+</button>
+      </div>
+      <button className=" w-32 h-12 border border-gray-300 rounded-md text-center  font-semibold tracking-wide" onClick={handleAddToCart}>Adicionar ao carrinho</button>
+      {stock === 0 && <p>Desculpe, sem estoque disponível.</p>}
+    </div>
+  );
+};
+
+export { ItemCount };
+
