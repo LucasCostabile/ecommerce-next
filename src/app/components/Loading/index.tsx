12ebacdbdeef97e852264e20e91855d185ab2027
Lucas Costabile <111201828+LucasCostabile@users.noreply.github.com>
2023-11-27 22:27:34 -0300

    update 1.2

--- src/app/components/Loading/index.tsx
+++ src/app/components/Loading/index.tsx
@@ -1,11 +1,18 @@
-const Loading = () => {
-
-    return (
-            <div className="h-screen bg-gray-950 opacity-50  top-0 left-0 flex justify-center items-center">
-                <div className="rounded-full w-16 h-16 border-t-4 border-l-4 border-blue-500 animate-spin"></div>
-            </div>
-    )
-
+interface Props {
+    loading: boolean;
+    nameScreen: string;
 }
 
-export  { Loading };
+const Loading: React.FC<Props> = ({ loading, nameScreen }) => {
+    return (
+        <>
+            {loading && (
+                <div className="w-full h-full bg-gray-950 opacity-50 fixed top-0 left-0 flex justify-center items-center">
+                    <div className="rounded-full w-16 h-16 border-t-4 border-l-4 border-blue-500 animate-spin"></div>
+                </div>
+            )}
+        </>
+    )
+}
+
+export { Loading }
