8c5707e9dd240a2e829dcbee38aca20f247e483e
Lucas Costabile <111201828+LucasCostabile@users.noreply.github.com>
2023-11-27 22:26:04 -0300

    update 1.2

--- src/app/components/ItemsContainer/ItemListContainer.tsx
+++ src/app/components/ItemsContainer/ItemListContainer.tsx
@@ -1,29 +1,24 @@
 'use client'
 import React from 'react';
-import { ItemCount } from '../Count/ItemCount';
+import { ItemList } from '../Item/ItemList';
 
 
 interface ItemListContainerProps {
   greeting: string;
 }
 
+
 const ItemListContainer: React.FC<ItemListContainerProps> = ({ greeting }) => {
-  const handleAddToCart = (quantity: number) => {
-    console.log(`Adicionar ao carrinho: ${quantity}`);
-  };
 
   return (
-    <div className='p-2'>
-      <div className="text-center">
-        <h2>{greeting}</h2>
-      </div>
-      <div>
-        <h3 className='h-20'>Detalhes do Item</h3>
-        <h4 className='h-10'>Imagem item</h4>
-        <ItemCount initial={0} stock={10} onAdd={handleAddToCart} />
-      </div>
-    </div>
+    <section className='text-center'>
+      <h2>{greeting}</h2>
+      <section>
+      <ItemList />
+      </section>
+    </section>
+    
   );
 };
 
-export { ItemListContainer };
+export { ItemListContainer }
