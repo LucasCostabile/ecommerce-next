d785e92c7833d15634116d3c2385c690b3b8114f
LucasCostabile <lucastc95@hotmail.com>
2023-12-19 21:51:11 -0300

    update

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -1,22 +1,23 @@
 import Link from "next/link";
 import { Cart } from "./CartWidget";
 
-const NavBar = () => {
-    return(
-        <div className="bg-gray-700 p-4 shadow-2xl flex justify-between">
-            <h1 className="text-center text-slate-200 font-bold ">ClickByte</h1>
 
-            <ul className="text-center flex gap-5">
-                <Link href="/">
+const NavBar = () => {
+    
+    return(
+        <div className="bg-gray-700 p-4  shadow-2xl flex justify-between">
+            <h1 className="text-center text-slate-200 font-bold ">ClickByte</h1>
+                <ul className="text-center flex gap-5 justify-around">
+                    <Cart />
                     <button className="ml-6 bg-slate-500 px-3 py-1 text-white rounded-lg hover:opacity-90">
-                    Inicio
+                    <Link href="/">Inicio</Link>
                     </button>
-                </Link>
-                <button className="ml-6 bg-slate-500 px-3 py-1 text-white rounded-lg hover:opacity-90">
-                    Login
-                </button>
-                <Cart />
-            </ul>
+                    <button className="ml-6 bg-slate-500 px-3 py-1 text-white rounded-lg hover:opacity-90">
+                        Login
+                    </button>
+                    
+                </ul>
+           
         </div>
         
     )
