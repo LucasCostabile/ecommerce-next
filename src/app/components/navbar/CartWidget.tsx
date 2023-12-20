b2a502ac95a7579ecc6c16dc12116e96742a4256
LucasCostabile <lucastc95@hotmail.com>
2023-12-19 21:51:29 -0300

    new component

--- src/app/components/navbar/CartWidget.tsx
+++ src/app/components/navbar/CartWidget.tsx
@@ -1,12 +1,29 @@
+'use client'
 import Carrinho from '@/app/assets/icon_carrinho.svg'
+import { useCartStore } from '@/app/store/store';
 import Image from 'next/image';
+import CartDrawer from '../Cart/CartDrawer';
 
 const Cart = () => {
-    return(
-    <button className='h-9 w-8'>
-       <Image src={Carrinho} alt="Carrinho"/>
-    </button>
-)
+    const useStore = useCartStore();
+
+    return (
+        <>
+            <div
+                onClick={() => useStore.toggleCart()}
+                className='relative'>
+                <button className='h-9 w-8'>
+                    <Image src={Carrinho} alt="Carrinho" />
+                </button>
+
+                <span className='bg-orange-500 text-sm text-bold rounded-full h-5 w-5 flex items-center justify-center absolute left-8 bottom-8'>
+                    {useStore.cart?.length}
+                </span>
+            </div>
+            {
+                !useStore.isOpen && (<CartDrawer />)}
+        </>
+    )
 }
 
 export { Cart }; 
