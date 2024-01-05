907c0c08486bb13f2a2b1b8e1f682b2c46891417
LucasCostabile <lucastc95@hotmail.com>
2024-01-05 10:40:21 -0300

    adc motionframe

--- src/app/components/Item/Item.tsx
+++ src/app/components/Item/Item.tsx
@@ -1,33 +1,38 @@
 // Item.tsx
+'use client'
 import React from 'react';
-import Image, { StaticImageData } from 'next/image';
+import Image from 'next/image';
 import  Button  from '../Button';
 import Link from 'next/link';
 import { Idetail } from '../interfaces/detail.interface';
 import AddCart from '@/app/components/Button/AddCart'
-import { useRouter } from 'next/navigation';
-
-
-
+import { motion } from 'framer-motion';
 
 const Item: React.FC<{ product: Idetail }> = ({ product }) => {
 
-  const { id, title, description, price, image } = product;
+  const { id, title, description, price, image, stock } = product;
 
   return (
-    <div className='flex flex-col shadow-lg h-96 items-center justify-center p-5 g-10'>
+    <motion.div 
+    initial={{opacity:0, x: -100}}
+    whileInView={{opacity:1, x: 0}}
+    exit={{ opacity: 0, x:-100}}
+    transition={{duration: 0.5}}
+    className='flex flex-col shadow-lg h-96 items-center justify-center p-5 g-10'>
+
         <div className='relative max-h-72 flex-1 '>
           <Image src={image!} alt={title!} width={180} height={180} />
         </div>
         <div className='flex flex-col justify-center items-center font-bold'>
-          <h3 className='my-1'>{description}</h3>
-          <p className='my-1'>Preço: {price}</p>
-          <Link href={`/ContainerDetail/${id}`}>
+          <h3 className='my-1 text-orange-800'>{title}</h3>
+          <p className='my-1 text-green-500 font-bold'>R$ {price}</p>
+          <p>quantidade em estoque: {stock}</p>
+          <Link href={`/ContainerDetail/${id}`} passHref>
             <Button   title='Ver detalhes do produto'/>
           </Link>
           <AddCart product={product} />
       </div>
-    </div>
+    </motion.div>
   );
 };
 
