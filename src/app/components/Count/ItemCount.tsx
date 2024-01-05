e08f05f1aaf8b025ec9192e97a968b1b8e8be5a4
LucasCostabile <lucastc95@hotmail.com>
2024-01-05 11:17:46 -0300

    update

--- src/app/components/Count/ItemCount.tsx
+++ src/app/components/Count/ItemCount.tsx
@@ -8,9 +8,6 @@ interface ItemCountProps {
 
 const ItemCount: React.FC<ItemCountProps> = ({ initial, stock, onAdd }) => {
   const [count, setCount] = useState(initial);
-  const [totalStock, setTotalStock] = useState(stock)
-  const [cart, setCart] = useState<number>(0);
-  
 
   const handleIncrement = () => {
     if (count < stock) {
@@ -27,39 +24,34 @@ const ItemCount: React.FC<ItemCountProps> = ({ initial, stock, onAdd }) => {
   const handleAddToCart = () => {
     if (stock > 0 && count > 0) {
       onAdd(count);
-      setTotalStock((prevStock => prevStock - count))
     }
   };
 
-  const updateCart = (quantity: number) => {
-    // Atualiza o estado do carrinho com a quantidade selecionada
-    setCart(cart + quantity);
-    // Atualiza o estoque disponível
-    setTotalStock(totalStock - quantity);
-  };
-
-  //console.log("Quantidade selecionada:", count); // Adiciona log da quantidade selecionada
-  //console.log("Quantidade no estoque:", totalStock);
-  //console.log("Condição de exibição:", stock === 0);
-  
   return (
-    <div className='flex flex-col  items-center'>
-      <h2>quantidade no estoque: {totalStock}</h2>
-      <div className=" flex justify-around items-center w-32 h-12  border border-gray-300 rounded-md font-bold">
-        <button className=" w-6 h-6 bg-slate-400 rounded-full " onClick={handleDecrement}>-</button>
+    <div className="flex flex-col items-center">
+      <div className="flex justify-around items-center w-32 h-12 border border-gray-300 rounded-md font-bold">
+        <button className="w-6 h-6 bg-slate-400 rounded-full" onClick={handleDecrement}>
+          -
+        </button>
         <p>{count}</p>
-        <button className=" w-6 h-6 bg-slate-400 rounded-full " onClick={handleIncrement} disabled={count >= totalStock ?  true  :  false } >+</button>
+        <button
+          className="w-6 h-6 bg-slate-400 rounded-full"
+          onClick={handleIncrement}
+          disabled={count >= stock ? true : false}
+        >
+          +
+        </button>
       </div>
-      <button className=" flex w-32 h-12 border border-gray-300 rounded-md font-semibold tracking-wide" 
-      onClick={handleAddToCart}
-      disabled={count > totalStock ?  true  :  false }
+
+      <button
+        className="flex w-32 h-12 border border-gray-300 rounded-md font-semibold tracking-wide"
+        onClick={handleAddToCart}
+        disabled={count > stock ? true : false}
       >
         Adicionar ao carrinho
       </button>
-      <p>Total no carrinho: {cart}</p>
-      {totalStock === 0 &&  <p>Desculpe, sem estoque disponível.</p>}
     </div>
   );
 };
- 
-export default ItemCount ;
+
+export default ItemCount;
