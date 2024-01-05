096938aa976717b43ad128014bd07aa8d33359cf
LucasCostabile <lucastc95@hotmail.com>
2024-01-05 10:42:14 -0300

    update

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -9,7 +9,7 @@ const Button = ({title}: Props) => {
 
     return(
         <>
-        <button  className=" flex justify-center items-center w-40 h-12 border border-gray-300 rounded-md font-semibold tracking-wide">
+        <button  className=" flex justify-center items-center w-40 h-12 border border-gray-300 rounded-md font-semibold tracking-wide bg-orange-600 text-white ">
         {title}
         </button>
         </>
