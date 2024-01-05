b3424cf6ad5e74175fe100187be560bc4850e9bf
LucasCostabile <lucastc95@hotmail.com>
2024-01-05 18:53:39 -0300

    included stock

--- src/app/components/ItemDetail/ItemDetail.tsx
+++ src/app/components/ItemDetail/ItemDetail.tsx
@@ -3,10 +3,10 @@ import { Idetail } from '../interfaces/detail.interface';
 import Image from 'next/image';
 import { useCartStore } from '@/app/store/store';
 
-const ItemDetail = ({ id, title, description, price, image }: Idetail) => {
+const ItemDetail = ({ id, title, description, price, image, stock }: Idetail) => {
   const useStore = useCartStore();
   const [addedToCart, setAddedToCart] = useState<number>(0); // Estado local para controlar a quantidade adicionada ao carrinho
-
+  
   const handleAddToCart = (item: Idetail) => {
     useStore.addProduct(item);
     setAddedToCart(addedToCart + 1); // Atualiza a quantidade adicionada localmente ao clicar em "Adicionar ao Carrinho"
@@ -27,13 +27,12 @@ const ItemDetail = ({ id, title, description, price, image }: Idetail) => {
 
   return (
     <section className="flex justify-center items-center">
-      <div 
-      className="items-center justify-center m-11 p-2 ">
+      <div className="items-center justify-center m-11 p-2 ">
         <Image src={image!} alt={title!} height={500} width={500}></Image>
       </div>
-      <div className="flex flex-col items-center justify-center p-10 gap-6 border-solid border-2 rounded-md border-gray-600 bg-gray-700">
+      <div className="flex flex-col items-center justify-center w-96 p-10 mr-1 gap-6 border-solid border-2 rounded-md border-gray-600 bg-gray-700">
         <h1 className=" text-orange-600 text-center m-10 text-lg font-semibold">Detalhes do produto:</h1>
-        <h2 className="text-center text-black font-bold">{description}</h2>
+        <h2 className="text-center text-orange-400 font-bold">{description}</h2>
         <p className="text-lg text-green-400 font-bold">R${price}</p>
         
         {/* Exibe a quantidade no carrinho */}
@@ -42,13 +41,13 @@ const ItemDetail = ({ id, title, description, price, image }: Idetail) => {
         <div>
           <button
             className="text-white py-1 px-2 border bg-orange-600 rounded-md mt-2 text-sm mr-1"
-            onClick={() => handleAddToCart({ id, title, description, price, image })}
+            onClick={() => handleAddToCart({ id, title, description, price, image, stock })}
           >
             Adicionar
           </button>
           <button
             className="text-white py-1 px-2 border bg-orange-600 rounded-md mt-2 text-sm mr-1"
-            onClick={() => handleDecrement({ id, title, description, price, image })}
+            onClick={() => handleDecrement({ id, title, description, price, image, stock })}
           >
             Remover
           </button>
