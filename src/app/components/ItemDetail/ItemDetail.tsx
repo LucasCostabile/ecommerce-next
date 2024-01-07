4c9fa4f7f8a905849162172e85b78f03b9788952
LucasCostabile <lucastc95@hotmail.com>
2024-01-07 16:27:16 -0300

    add controle de stock

--- src/app/components/ItemDetail/ItemDetail.tsx
+++ src/app/components/ItemDetail/ItemDetail.tsx
@@ -1,11 +1,16 @@
-import { useState } from 'react'; 
+import { useEffect, useState } from 'react'; 
 import { Idetail } from '../interfaces/detail.interface';
 import Image from 'next/image';
 import { useCartStore } from '@/app/store/store';
 
 const ItemDetail = ({ id, title, description, price, image, stock }: Idetail) => {
   const useStore = useCartStore();
+  const [currentStock, setCurrentStock] = useState(stock)
   const [addedToCart, setAddedToCart] = useState<number>(0); // Estado local para controlar a quantidade adicionada ao carrinho
+
+  useEffect(() => {
+    setCurrentStock(stock)
+  }, [stock])
   
   const handleAddToCart = (item: Idetail) => {
     useStore.addProduct(item);
@@ -34,14 +39,19 @@ const ItemDetail = ({ id, title, description, price, image, stock }: Idetail) =>
         <h1 className=" text-orange-600 text-center m-10 text-lg font-semibold">Detalhes do produto:</h1>
         <h2 className="text-center text-orange-400 font-bold">{description}</h2>
         <p className="text-lg text-green-400 font-bold">R${price}</p>
+        <p className=" text-white font-bold">quantidade disponível em estoque: {currentStock}</p>
         
         {/* Exibe a quantidade no carrinho */}
-        <p className='text-black font-bold'>Adicionado ao carrinho: {quantityInCart}</p>
+        <p className='text-white font-bold'>Adicionado ao carrinho: {quantityInCart}</p>
         {/* Botões para adicionar/remover do carrinho */}
         <div>
           <button
             className="text-white py-1 px-2 border bg-orange-600 rounded-md mt-2 text-sm mr-1"
-            onClick={() => handleAddToCart({ id, title, description, price, image, stock })}
+            onClick={() => { 
+            handleAddToCart({ id, title, description, price, image, stock })
+            setCurrentStock(currentStock! - 1)
+          }}
+          disabled={currentStock! <= 0}
           >
             Adicionar
           </button>
